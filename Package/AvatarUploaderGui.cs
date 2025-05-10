a19c3bef08aab986ddbe6befbdb0e0d9bec0270e
neon-glowstick <neon.glowstick@proton.me>
2025-05-10 16:29:22 +0200

    Begin Gui for uploading avatars

---
+++ Package/AvatarUploaderGui.cs
@@ -0,0 +1,68 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace NeonGlowstick.BasisVr.GDriveHosting
+{
+    [InitializeOnLoad]
+    public static class AvatarUploaderGui
+    {
+        private const string AuthUrl = "https://neon-glowstick.github.io/BasisVR-GDriveHosting";
+
+        static AvatarUploaderGui()
+        {
+            BasisAvatarSDKInspector.InspectorGuiCreated += OnInSpectorGuiCreated;
+        }
+
+        private static void OnInSpectorGuiCreated(BasisAvatarSDKInspector inspector)
+        {
+            inspector.rootElement.Add(BuildGui(inspector));
+        }
+
+        private static VisualElement BuildGui(BasisAvatarSDKInspector inspector)
+        {
+            var container = new VisualElement();
+            container.Add(new Label
+            {
+                text = "Google Drive uploader"
+            });
+            container.Add(new Button(() =>
+            {
+                Application.OpenURL(AuthUrl);
+            })
+            {
+                text = "Open auth page"
+            });
+
+            // Todo: store and retrieve token in a file between editor reloads
+            var token = string.Empty;
+            var accessTokenField = PasswordField("OAuth token", token);
+            accessTokenField.RegisterValueChangedCallback(value => token = value.newValue);
+            container.Add(accessTokenField);
+
+            var uploadButton = new Button
+            {
+                text = "Upload to drive"
+            };
+            uploadButton.clicked += () =>
+            {
+                uploadButton.SetEnabled(false);
+                var avatarName = inspector.Avatar.BasisBundleDescription.AssetBundleName;
+                // todo: Upload
+                uploadButton.SetEnabled(true);
+            };
+
+            container.Add(uploadButton);
+            return container;
+        }
+
+        private static TextField PasswordField(string label, string value)
+        {
+            return new TextField(-1, false, true, '•')
+            {
+                label = label,
+                value = value
+            };
+        }
+    }
+}
